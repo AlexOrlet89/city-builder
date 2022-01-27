5e25355e2afc35ad860b6865f9674111ef849868
Alex Orlet <AlexOrlet89@gmail.com>
2022-01-26 17:44:25 -0800

    grabbed a bunch of DOM elements

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 // import functions and grab DOM elements
 
+//Selector Elements
+const landmarksSelection = document.getElementById('Landmarks-Selection');
+const restaurantsSelection = document.getElementById('Restaurants-Selection');
+const shoppingSelection = document.getElementById('Shopping-Selection');
+
+//Elements with Images
+const landmarksImg = document.getElementById('LandmarksImg');
+const restaurantsImg = document.getElementById('RestaurantsImg');
+const shoppingImg = document.getElementById('ShoppingImg');
+
+//Slogan Elements
+const sayingsSection = document.getElementById('Sayings-Section');
+const sayingsInput = document.getElementById('sayings-input');
+const sayingsButton = document.getElementById('sayings-button');
 // let state
 
 // set event listeners 
